9ebec638fbd31f1ba5586fd1654e247f3b57868c
Kernionby4145 <47937024+Kernionby4145@users.noreply.github.com>
2019-03-11 00:36:26 -0400

    up

--- css/managerb/index.js
+++ css/managerb/index.js
@@ -46,7 +46,7 @@ function login(){
 function logout(){
 
   firebase.auth().signOut();
-  window.location = "/Webserver/index.html"
+  window.location = "https://kernion-by-4145.github.io/Webserver/"
 
 }
 
