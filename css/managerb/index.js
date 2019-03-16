7e5937c2d9f05263844a35e7cd44fb0a28438880
Kernionby4145 <47937024+Kernionby4145@users.noreply.github.com>
2019-03-16 16:38:05 -0400

    ij

--- css/managerb/index.js
+++ css/managerb/index.js
@@ -46,7 +46,7 @@ function login(){
 function logout(){
 
   firebase.auth().signOut();
-  window.location.replace("https://kernion-by-4145.github.io/Webserver/");
+  window.location.replace("https://kernion-by-4145.github.io/Kernion/");
 
 }
 
